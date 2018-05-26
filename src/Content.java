a566afacde1055adeefcc2db4442911ba434f28a
张君 <1028191676@qq.com>
2018-05-27 01:09:52 +0800

    ..

--- src/Content.java
+++ src/Content.java
@@ -2,8 +2,8 @@ public class Content {
     public static void main(String[] args) throws Exception {
         new Creater("jdbc:mysql://localhost:3306/test_bean","root",null)
                 .addTable("tt_")
-                .setBeanPackage("org.zj.demo")
-                .setMapperPackage("org.zj.dao")
+                .setBeanPackage("org.zj.demo.bean")
+                .setMapperPackage("org.zj.demo.dao")
                 .handle();
     }
 }
